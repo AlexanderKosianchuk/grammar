047d216ab6bb0544908a5d3ce14a1c80eee1a20f
Stasananas62 <stasananas62@gmail.com>
2017-04-27 15:56:04 +0300

    Top menu init

--- components/Home.js
+++ components/Home.js
@@ -2,6 +2,10 @@
 
 import React, { Component } from 'react';
 import { View, Text, Button, TextInput, StyleSheet, AsyncStorage } from 'react-native';
+import {navbar} from 'react-native';
+import { Navigator, Image, TouchableHighlight } from 'react-native';
+import TopMenu from './TopMenu';
+
 
 const STORAGE_PREFIX = '@QarSyncManagerFiles:files';
 
@@ -21,29 +25,36 @@ class Home extends React.Component {
                 this.setState({file: this.state.inputText});
             });
     }
-
     render() {
         let Actions = this.props.routes;
         return (
-            <View style={styles.container}>
-                <Text>Home page</Text>
-                <Button onPress={ Actions.settings }  title={ this.props.btnText } >Go settings</Button>
-                <Text style={styles.filesTitle}>Files:</Text>
-                <Text>{ this.state.file }</Text>
-                <TextInput style={ styles.inputFileName }
-                    autoCapitalize="none"
-                    value={this.state.inputText}
-                    onChangeText={(text) => this.setState({inputText: text})}
-                />
-                <Button onPress={ this.addFile.bind(this) }  title="addFile" >Add File</Button>
+            <View>
+              <TopMenu/>
+
+              <View style={styles.container}>
+
+                <Button  title='QAR' ></Button>
+                <Button  title='SNfS'></Button>
+
+                  <Button onPress={ Actions.settings }  title={ this.props.btnText } >Go settings</Button>
+                  <Text>Home page</Text>
+                  <Text style={styles.filesTitle}>Files:</Text>
+                  <Text>{ this.state.file }</Text>
+                  <TextInput style={ styles.inputFileName }
+                      autoCapitalize="none"
+                      value={this.state.inputText}
+                      onChangeText={(text) => this.setState({inputText: text})}
+                  />
+                  <Button onPress={ this.addFile.bind(this) }  title="addFile" >Add File</Button>
+                </View>
             </View>
         );
     }
 }
 
+
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#55efef',
--- components/TopMenu.js
+++ components/TopMenu.js
@@ -1,9 +1,35 @@
 'use strict';
 
 import React, { Component } from 'react';
+import NavigationBar from 'react-native-navbar';
+import { View, Text } from 'react-native';
 
 export default class TopMenu extends React.Component {
     render(){
-        return ();
+        return (
+          <View style={styles.container}>
+            <Text>'1111'</Text>
+          </View>
+        );
     }
 }
+const styles = {
+  container: {
+    top: 0,
+    backgroundColor: '#ee1',
+    alignSelf: 'stretch',
+    textAlign: 'center',
+  },
+};
+
+const rightButtonConfig = {
+  title: 'Next',
+  handler: () => alert('hello!'),
+};
+
+const titleConfig = {
+  title: 'Hello, world',
+};
+
+
+module.exports = TopMenu;
