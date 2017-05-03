bf2665838fa43a8609b1da9c0d8c582e493de6d3
AlexanderKosianchuk <alexanderkosianchuk@luch15.com>
2017-05-03 13:21:16 +0300

    reset top menu unauthorized commits

--- components/Home.js
+++ components/Home.js
@@ -2,14 +2,6 @@
 
 import React, { Component } from 'react';
 import { View, Text, Button, TextInput, StyleSheet, AsyncStorage } from 'react-native';
-import {navbar} from 'react-native';
-import { Navigator, Image, TouchableHighlight } from 'react-native';
-import TopMenu from './TopMenu';
-import { Col, Row, Grid } from "react-native-easy-grid";
-
-import Icon from 'react-native-vector-icons/FontAwesome';
-const myIcon3 = (<Icon name="home" size={60} color="#708090" />)//home
-
 
 const STORAGE_PREFIX = '@QarSyncManagerFiles:files';
 
@@ -29,37 +21,33 @@ class Home extends React.Component {
                 this.setState({file: this.state.inputText});
             });
     }
+
     render() {
         let Actions = this.props.routes;
         return (
-            <View style={{flex:1,flexDirection: 'column',}}>
-              <TopMenu settingsActions={ Actions.settings }/>
-              <View style={styles.container}>
-              <Text>{myIcon3}</Text>
-                  <Text>Home page</Text>
-                  <Text style={styles.filesTitle}>Files:</Text>
-                  <Text>{ this.state.file }</Text>
-                  <TextInput style={ styles.inputFileName }
-                      autoCapitalize="none"
-                      value={this.state.inputText}
-                      onChangeText={(text) => this.setState({inputText: text})}
-                  />
-                  <Button onPress={ this.addFile.bind(this) }  title="addFile" >Add File</Button>
-                </View>
+            <View style={styles.container}>
+                <Text>Home page</Text>
+                <Button onPress={ Actions.settings }  title={ this.props.btnText } >Go settings</Button>
+                <Text style={styles.filesTitle}>Files:</Text>
+                <Text>{ this.state.file }</Text>
+                <TextInput style={ styles.inputFileName }
+                    autoCapitalize="none"
+                    value={this.state.inputText}
+                    onChangeText={(text) => this.setState({inputText: text})}
+                />
+                <Button onPress={ this.addFile.bind(this) }  title="addFile" >Add File</Button>
             </View>
         );
     }
 }
 
-
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-        flexDirection: 'column',
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F0FFFF',
-      },
+        backgroundColor: '#55efef',
+    },
     filesTitle: {
         margin: 25,
     },
--- components/TopMenu.js
+++ components/TopMenu.js
@@ -1,34 +1,9 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-const myIcon = (<Icon name="map-signs" size={60} color="#F0FFFF" />)//переключение языка
-const myIcon2 = (<Icon name="repeat" size={60} color="#F0FFFF" />)//подключение к QAR
-const myIcon3 = (<Icon name="cogs" size={60} color="#708090" />) // страница настроек
-const myIcon4 = (<Icon name="plus" size={60} color="#F0FFFF" />)//оптравка новых файлов на сервер
-import { Col, Row, Grid } from "react-native-easy-grid";
-
 
 export default class TopMenu extends React.Component {
-  render() {
-        return (
-          <View style={{backgroundColor: '#191970',flexDirection: 'row',justifyContent: 'space-between',}}>
-            <Text>{myIcon2}</Text>
-            <Text>{myIcon}</Text>
-            <Text>{myIcon4}</Text>
-          <Text onPress={ this.props.settingsActions }  title={ myIcon3 } >{myIcon3}</Text>
-          </View>
-        );
+    render(){
+        return ();
     }
 }
-
-/*
-backgroundColor: '#ee1',
-alignSelf: 'stretch',
-textAlign: 'center',
-onPress={ Actions.settings }
-*/
-
-module.exports = TopMenu;
-//rocket
