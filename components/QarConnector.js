03515e316761c588554d7cfccd8864d61f7abcab
hp <kostyasharp787898@gmail.com>
2017-05-18 13:02:10 +0300

    added passing http auth using new component and fetch

---
+++ components/QarConnector.js
@@ -0,0 +1,21 @@
+'use strict';
+import base64 from 'base-64'
+
+// object to connect to remoted server
+var QarConnector = {
+    connect: function(qarTokenUrl, clientID, clientSecret) {
+        fetch(qarTokenUrl, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type' : 'application/text',
+                "Authorization" : "Basic " + base64.encode(clientID + ":" + clientSecret)
+            }
+        })
+        .then (function(response){
+            console.log(response)
+        })
+   }
+}
+
+export default QarConnector;
