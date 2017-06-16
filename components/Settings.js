c9f7656d376a1a43155bf09fce0dd450799cd167
Stasananas62 <stasananas62@gmail.com>
2017-06-16 13:10:12 +0300

    SettingsPage_afterCorect

--- components/Settings.js
+++ components/Settings.js
@@ -16,98 +16,101 @@ const configArr = [
     {label: 'syncServerPass', option: 'syncServerPass', valueText:'myPassServer****'},
 ];
 
-class Settings extends React.Component {
-  constructor(props) {
-    super(props);
+class Settings extends React.Component
+{
 
-    let stateValues = {};
+  constructor(props)
+  {
+      super(props);
 
-    configArr.forEach((currentValue) => {
-         stateValues[currentValue.option] = currentValue.valueText;
-    });
+      let stateValues = {};
 
-    this.state = stateValues;
+      configArr.forEach((currentValue) => {
+           stateValues[currentValue.option] = currentValue.valueText;
+      });
+
+      this.state = stateValues;
   }
-  
+
   buildOptions(){
-    let configInputs = [];
-    let that = this;
+      let configInputs = [];
+      let that = this;
 
-    configArr.forEach((currentValue) => {
-           configInputs.push(that.putTextInput(currentValue.label, currentValue.option, currentValue.valueText));
-    });
+      configArr.forEach((currentValue) => {
+             configInputs.push(that.putTextInput(currentValue.label, currentValue.option, currentValue.valueText));
+      });
 
-    return(configInputs);
-  };
+      return(configInputs);
+    };
 
   onButtonPress(){
 
-    configArr.forEach((currentValue) => {
-         AsyncStorage.getItem(STORAGE_PREFIX + currentValue.option, currentValue.value);
-    });
-        console.log(this.state);
-    }
+      configArr.forEach((currentValue) => {
+           AsyncStorage.getItem(STORAGE_PREFIX + currentValue.option, currentValue.value);
+      });
+          console.log(this.state);
+      }
 
   handleChange(text, option)
-  {
-    this.setState({
-        [option]: text
-    });
-    console.log(text);
-  }
+    {
+        this.setState({
+            [option]: text
+        });
+        console.log(text);
+    }
 
   putTextInput(label, option, valueText) {
-    return(
-      <View>
-        <Text style={styles.inscription} key={option+'text'} > { label }:</Text>
-           <TextInput
-              key={option} value={this.state[option]} onChangeText={ (text) => this.handleChange(text, option) }
-           />
-      </View>
-    );
-  }
+      return(
+          <View>
+              <Text style={styles.inscription} key={option+'text'} > { label }:</Text>
+                 <TextInput
+                      key={option} value={this.state[option]} onChangeText={ (text) => this.handleChange(text, option) }
+                  />
+          </View>
+      );
+    }
 
     render() {
-      let options = this.buildOptions();
-      var Actions = this.props.routes;
-        return (
-          <View style={ styles.container }>
-            <View style={ styles.settingsPage }>
-              <Text>{settingsPageIconIcon}</Text>
-              <Text>Settings page</Text>
-                <View style={ styles.buttons } >
-                  <Button onPress={ Actions.home } title={ this.props.btnText }>Go home</Button>
-                  <Button onPress={this.onButtonPress.bind(this)} title="Save" />
+        let options = this.buildOptions();
+        var Actions = this.props.routes;
+          return (
+              <View style={ styles.container }>
+                  <View style={ styles.settingsPage }>
+                      <Text>{settingsPageIconIcon}</Text>
+                      <Text>Settings page</Text>
+                        <View style={ styles.buttons } >
+                            <Button onPress={ Actions.home } title={ this.props.btnText }>Go home</Button>
+                            <Button onPress={this.onButtonPress.bind(this)} title="Save" />
+                        </View>
+                  </View>
+                      <View >
+                          { options }
+                      </View>
                 </View>
-            </View>
-               <View >
-                 { options }
-               </View>
-          </View>
-       );
+          );
     }
-}
+  }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#efef55',
-    },
-  buttons:{
-    flex:1,
-    flexDirection: 'column',
-    },
-  settingsPage:{
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 10,
-    },
-    text: {
-   color: 'black',
- },
-  });
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#efef55',
+  },
+  buttons: {
+      flex:1,
+      flexDirection: 'column',
+  },
+  settingsPage: {
+      flex:1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: 10,
+  },
+  text:{
+     color: 'black',
+  },
+});
 
 
 module.exports = Settings;
